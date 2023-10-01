79203ffbfd0d0ade72591d34c1a1ead352cfe960
matheusgusmao1 <146306575+matheusgusmao1@users.noreply.github.com>
2023-10-01 17:54:25 -0300

    adicionada a opção de sair do sistema
    foi adicionado ao sistema a opção de saída para o usuário.

--- cartorioEbacRegistrarConsultar.cpp
+++ cartorioEbacRegistrarConsultar.cpp
@@ -126,6 +126,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção:");											//fim do menu principal
 	
 	
@@ -145,6 +146,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema.\n");
+			return 0;
 			break;
 			
 			default:
